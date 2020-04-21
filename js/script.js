4593438839d783989876a645a94ac2a5bf6ab3fe
KevinBarreraIIT <60115269+KevinBarreraIIT@users.noreply.github.com>
2020-04-21 15:37:46 -0500

    added js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-
- 
-
-
-
-
-
-
+    var input;
+    var button = document.getElementById("entrybutton");
+    var h2 = document.getElementById("textoutput");
+    button.addEventListener("click", function()
+    {
+      input = document.getElementById("entryinput").value;
+      alert("Kevin Barrera: " + input);
+      h2.innerHTML = input;
+    });
+  }
 
 window.addEventListener('load', init);
